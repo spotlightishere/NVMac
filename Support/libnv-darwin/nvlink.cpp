a30bd571a7b2979e69b8b7beb2ff5aac86b6ffb2
Spotlight <spotlight@joscomputing.space>
2025-06-07 19:41:08 -0500

    libnv-kernel: Enable debugging

--- Support/libnv-darwin/nvlink.cpp
+++ Support/libnv-darwin/nvlink.cpp
@@ -10,12 +10,56 @@
 #include <string>
 
 #include "nv_darwin.h"
+#include <nvlink_os.h>
 
 extern "C" {
 
 // Defined within `libc.cpp`.
 NV_STATUS os_alloc_mem(void** address, NvU64 size);
 void os_free_mem(void* address);
+
+// Taken directly from kernel-open.
+#define MAX_ERROR_STRING 512
+
+void nvlink_print(const char* file, int line, const char* function,
+                  int log_level, const char* fmt, ...) {
+    va_list arglist;
+    char nv_string[MAX_ERROR_STRING];
+    const char* sys_log_level;
+
+    // TODO(spotlightishere): We lack a concept of log levels.
+    switch (log_level) {
+    case NVLINK_DBG_LEVEL_INFO:
+        sys_log_level = "[NVLINK INFO] ";
+        break;
+    case NVLINK_DBG_LEVEL_SETUP:
+        sys_log_level = "[NVLINK DEBUG] ";
+        break;
+    case NVLINK_DBG_LEVEL_USERERRORS:
+        sys_log_level = "[NVLINK NOTICE] ";
+        ;
+        break;
+    case NVLINK_DBG_LEVEL_WARNINGS:
+        sys_log_level = "[NVLINK WARNING] ";
+        ;
+        break;
+    case NVLINK_DBG_LEVEL_ERRORS:
+        sys_log_level = "[NVLINK ERROR] ";
+        ;
+        break;
+    default:
+        sys_log_level = "[NVLINK INFO] ";
+        ;
+        break;
+    }
+
+    va_start(arglist, fmt);
+    vsnprintf(nv_string, sizeof(nv_string), fmt, arglist);
+    va_end(arglist);
+
+    nv_string[sizeof(nv_string) - 1] = '\0';
+    nvd_log("%snvidia-nvlink: %s", sys_log_level, nv_string);
+}
 
 void* nvlink_malloc(NvLength size) {
     void* ptr;
@@ -32,8 +76,8 @@ void nvlink_assert(int expression) {
     nvd_log("NVLink: Assertion failed!\n");
 }
 
-void nvlink_memcpy(void* dst, const void* src, NvLength len) {
-    memcpy(dst, src, len);
+void* nvlink_memcpy(void* dst, const void* src, NvLength len) {
+    return memcpy(dst, src, len);
 }
 
 void* nvlink_memset(void* dest, int value, NvLength size) {
